e38be1914af047068d7fefa99faa9c71fc7c34c0
Joseph Angeletti <jangelet@gmail.com>
2018-02-02 13:26:56 -0500

    Solution to isEverywhere

---
+++ java/array-2/isEverywhere.java
@@ -0,0 +1,7 @@
+public boolean isEverywhere(int[] nums, int val){
+	boolean inPairs = true;
+	for(int i = 0; i < nums.length - 1; i++){
+		if(nums[i] != val && nums[i+1] != val) inPairs = false;
+	}
+	return inPairs;
+}
