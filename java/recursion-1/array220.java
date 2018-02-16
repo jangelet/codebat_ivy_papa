5ee2c3272e5a3bff616d118bf0d9f6c78dd8c839
Joseph Angeletti <jangelet@gmail.com>
2018-02-16 13:21:29 -0500

    Solution to array220

---
+++ java/recursion-1/array220.java
@@ -0,0 +1,5 @@
+public boolean array220(int[] nums, int index) {
+	if(index == nums.length - 1 || nums.length < 2) return false;
+	if(nums[index]*10 == nums[index+1]) return true;
+	return array220(nums, index + 1);
+}
