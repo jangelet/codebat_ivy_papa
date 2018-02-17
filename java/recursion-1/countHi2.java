fd9a0c7af7934a123877de9d712c33cce9f0a851
Joseph Angeletti <jangelet@gmail.com>
2018-02-17 17:20:55 -0500

    Solution to countHi2

---
+++ java/recursion-1/countHi2.java
@@ -0,0 +1,8 @@
+public int countHi2(String str) {
+	if(!str.contains("hi")) return 0;
+	if(str.length() < 2) return 0;
+	if(str.charAt(0) == 'x' && str.substring(1,3).equals("hi")) return countHi2(str.substring(3));
+	if(str.substring(0,2).equals("hi")) return 1 + countHi2(str.substring(2));
+	return countHi2(str.substring(1));
+}
+
